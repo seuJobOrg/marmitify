a98693454c014da8ccbe2ff3092327dea62a0540
Silas Eduardo <silasn1@live.com>
2025-06-29 14:24:02 -0300

    cadastro_do_chef_backend

--- marmitify-backend/src/modules/chef/dto/create-chef-dto.ts
+++ marmitify-backend/src/modules/chef/dto/create-chef-dto.ts
@@ -1,8 +1,44 @@
+import { IsString, IsOptional, IsNotEmpty, MaxLength, IsBoolean } from 'class-validator';
+
 export class CreateChefDto {
+    @IsString()
+    @IsOptional()
+    @MaxLength(15)
     phone: string;
+
+    @IsString()
+    @IsOptional()
     bio: string;
+
+    @IsString()
+    @IsOptional()
     avatar: string;
+
+    @IsBoolean()
+    @IsOptional()
     isActive: boolean;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(100)
+    fullName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(50)
+    cuisineType: string;
+
+    @IsString()
+    @IsNotEmpty()
+    experience: string;
+
+    @IsString()
+    @IsOptional()
+    profilePicture?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    briefDescription: string;
     
     constructor(partial: Partial<CreateChefDto>) {
         Object.assign(this, partial);
