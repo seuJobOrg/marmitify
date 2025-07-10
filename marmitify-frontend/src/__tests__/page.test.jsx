403c344dcf8e84cd7a239b474e7c4b0648178271
Caio Rebert <caiorebert070802.cr@gmail.com>
2025-07-09 23:03:10 -0300

    Ajustando teste para funcionar com sessões

--- marmitify-frontend/src/__tests__/page.test.jsx
+++ marmitify-frontend/src/__tests__/page.test.jsx
@@ -1,10 +1,15 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Page from '../app/page'
+import { SessionProvider } from 'next-auth/react'
  
 describe('Page', () => {
   it('renders a heading', () => {
-    render(<Page />)
+    render(
+      <SessionProvider>
+        <Page />
+      </SessionProvider>
+    )
  
     const heading = screen.getByRole('heading', { level: 1 })
 
